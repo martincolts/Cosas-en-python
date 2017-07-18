e572e8eabaadb3f7fe93a296fb8355abd50572b1
martincolts <martinlopez89.quequen@gmail.com>
2017-07-18 00:08:20 -0300

    Creado el programa para insertar el nivel de la bateria y 0 o 1 dependiendo de si el wifi esta encendido o no

--- test/test.py
+++ test/test.py
@@ -1,6 +1,28 @@
 import csv
-with open('/home/tincho/Escritorio/0269e21475ac65ad57fbec9c83ab4960ec66581b.csv', 'rb') as csvfile:
+with open("00f1221ebf94129c8d4643d1542ebd4c1fabf9d5.csv", 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    bateryLevel = 0
+    wifiStatus = 0
+    list = []
+    rowList = []
     for row in spamreader:
-        print (row[3])
+        if "power" in row[3]:
+            if "level" in row[3]:
+                 bateryLevel = row[4]
+        if "wifi" in row[3]:
+            if "state" in row[3]:
+                if "enabled" in row[4]:
+                    wifiStatus = 1
+                else:
+                    wifiStatus = 0
+                print (bateryLevel,";",row[3],row[4],wifiStatus)
+                rowList.append(bateryLevel)
+                rowList.append(wifiStatus)
+                print(rowList)
+                list.append(rowList)
+                rowList = []
 
+with open('result.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for rowL in list:
+        spamwriter.write(rowL[0], rowL[1])
